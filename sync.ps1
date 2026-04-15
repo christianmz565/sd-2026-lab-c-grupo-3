946bffb835879f1f2a266a7d910bd0f254bb14aa
christianmz565 <christianmz565@gmail.com>
2026-04-15 17:53:05 -0500

    fix: wrong powershell sync name

--- sync.ps1
+++ sync.ps1
@@ -1,5 +1,5 @@
 mutagen sync create `
-  --name "$env:USERNAME-zed" `
+  --name "$env:USERNAME-sd-lab-c" `
   -c mutagen-settings.yml `
   ./ `
   vscode@dev.mistercricro8.top:28009:/home/vscode/sd-2026-lab-c-grupo-3
